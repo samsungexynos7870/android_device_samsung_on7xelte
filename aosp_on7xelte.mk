b51b2a12a9181ae6e5268c69e3de4bd7c854a52d
Francesco <60934657+FraSharp@users.noreply.github.com>
2021-01-19 08:34:19 +0100

    Update aosp_on7xelte.mk

--- aosp_on7xelte.mk
+++ aosp_on7xelte.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/on7xelte/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := on7xelte
