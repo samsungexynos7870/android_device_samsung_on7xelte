6d8e500009b64f9859a68c51448c40f6d1387812
Brian Gotte <bngotte@gmail.com>
2021-02-17 21:17:48 -0300

    on7xelte: Force GPU instead HWC

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -5,3 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Fix for echo in calls
 PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.disable_two_mic=false
+
+# Force GPU instead HWC
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwc.force_gpu=1
